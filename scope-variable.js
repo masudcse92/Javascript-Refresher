2c8b42e1665a85655156fc9670d87528cab3cc32
masudcse92 <masudcse9292@gmail.com>
2025-04-29 00:05:24 +0600

    var, let, const variable

---
+++ scope-variable.js
@@ -0,0 +1,11 @@
+var lang = " Java";
+
+function learn(topic){
+   var lang = topic;
+
+    console.log(` I'm learning ${topic}`);
+}
+
+learn ("Javascript");
+
+console.log(`I Know ${lang}`);
---
+++ script.js
@@ -0,0 +1,19 @@
+var lang = " Java";
+
+function learn(topic){
+   var lang = topic;
+
+   if (true){
+    
+    let a = 6;
+    a = 5;
+
+    console.log(a);
+   }
+
+    console.log(` I'm learning ${topic}`);
+}
+
+learn ("Javascript");
+
+console.log(`I Know ${lang}`);
