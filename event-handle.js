d3556ebd5fc55e6fa6c311ece24535486e70e58c
masudcse92 <masudcse9292@gmail.com>
2025-05-01 00:00:29 +0600

    Event handler

---
+++ event-handle.js
@@ -0,0 +1,14 @@
+// function hello(){
+//     console.log("I love you Javascript");
+// }
+
+// select button
+const button = document.getElementById("button");
+
+// event handler
+
+function hello(){
+    console.log("I love you JS");
+}
+
+button.addEventListener("click", hello);
