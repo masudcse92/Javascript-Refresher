edff965b84be550d507087e57cc4f1243069448b
masudcse92 <masudcse9292@gmail.com>
2025-05-01 01:01:16 +0600

    Array mathods

---
+++ array-mathod.js
@@ -0,0 +1,40 @@
+// array methods
+
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango", "Lemon"];
+
+// array find()
+// const result = fruits.find( (f) => f === "Orange" || f === "Mango");
+
+// findIndex()
+// const result = fruits.findIndex( (f) => f === "Orange");
+
+// filter()
+// const result = fruits.filter( (f) => f === "Orange" || f === "Apple");
+
+// slice()
+// const result = fruits.slice(2, 4);
+
+// splice()
+// const result = fruits.splice(2, 2, "Pineapple", "Fruit", "Furit 2");
+
+// concate();
+// const result = fruits.concat(["Fruit 1", "Fruit 2"]);
+
+// push()
+// const result = fruits.push("Fruit 1");
+
+// map()
+// const result = fruits.map( (f) => f === "Lemon");
+
+// forof
+
+// const number = [1, 2, 3, 4, 5];
+const name = "Masud";
+
+for ( c of name) {
+    console.log(c);
+}
+
+
+// console.log(result);
+console.log(fruits);
